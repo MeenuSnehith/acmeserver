8ca00a8e55783f7825bb24b7ea099beeb5015350
MeenuSnehith <myidismeenu@gmail.com>
2023-08-01 13:44:53 -0500

    removed redundant files

--- acme_backend/src/models/User.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) =>
-    sequelize.define('User', {
-        username: {
-            type: DataTypes.STRING,
-            unique: true
-        },
-        password: DataTypes.STRING,
-        permission: DataTypes.STRING
-    })
